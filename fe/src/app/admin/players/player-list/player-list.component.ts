d652f0c4b807131b160d8bc326a754899c7a9fde
Oz <enquiries@ozharrison.me>
2026-04-23 17:27:12 +0100

    admin players display

--- fe/src/app/admin/players/player-list/player-list.component.ts
+++ fe/src/app/admin/players/player-list/player-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { RouterLink } from '@angular/router';
 import { AdminPlayerService } from '../../../core/services/admin/admin-player.service';
+import { LoadSignalDirective } from '../../../shared/directives/load-signal.directive';
 
 @Component({
   selector: 'app-player-list',
-  imports: [],
+  imports: [LoadSignalDirective, RouterLink],
   templateUrl: './player-list.component.html',
   styleUrl: './player-list.component.scss',
 })
